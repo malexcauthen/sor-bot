abf09f0f892fdbbfef58e53ff743f2d74a9e5b3b
Alex Cauthen <malexcauthen@gmail.com>
2020-05-03 15:40:13 -0500

    Updated code to correspond to data changes

--- commands/war-report.js
+++ commands/war-report.js
@@ -27,7 +27,7 @@ module.exports = {
         return sendReports(message, reports);
       })
       .catch((error) => {
-        console.log(JSON.stringify(error));
+        console.log(error);
       });
   }
 };
@@ -39,23 +39,29 @@ const parseResponse = (res) => {
 };
 
 const createEmbedReports = (data) => {
-  let reports = [warReport()];
+  const reports = ['keeps', 'forts', 'cities', 'zonelocks'].reduce(
+    (acc, cur) => {
+      return acc.concat(reportMap(cur, data));
+    },
+    []
+  );
 
-  if (data.length === 4) {
-    reports = [...reports, city(data)];
-  } else {
-    for (let row of data) {
-      if (Forts[row.name]) {
-        reports = [...reports, fort(row)];
-      } else if (Pairing.has(row.name)) {
-        reports = [...reports, pairingLocked(row)];
-      } else {
-        reports = [...reports, keep(row)];
-      }
-    }
+  return [warReport()].concat(reports);
+};
+
+const reportMap = (key, data) => {
+  if (key === 'keeps') {
+    return keep(data[key]);
+  } else if (key === 'forts') {
+    return fort(data[key]);
+  } else if (key === 'cities') {
+    return city(data[key]);
+  } else if (key === 'zonelocks') {
+    return zoneLocks(data[key]);
   }
 
-  return reports;
+  console.log(`Unknown property: ${key}`, JSON.stringify(data[key]));
+  return [];
 };
 
 const warReport = () => {
@@ -67,92 +73,94 @@ const warReport = () => {
     );
 };
 
-const pairingLocked = (data) => {
-  return new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(`${data.name}`)
-    .setDescription(`${data.name} has been locked by **${data.owner}.**`);
-};
-const keep = (data) => {
-  const aao =
-    data.aaoOwner === 'Destruction' || data.aaoOwner === 'Order'
-      ? `${data.aao}% ${data.aaoOwner}`
-      : data.aaoOwner;
-
-  const orderKeep = data.keep1.owner === 'Order' ? 1 : 2;
-  const destroKeep = data.keep1.owner === 'Order' ? 2 : 1;
-
-  const orderKeepStatus =
-    data[`keep${orderKeep}`].obj === 'Safe'
-      ? data[`keep${orderKeep}`].obj
-      : `${data[`keep${orderKeep}`].status} ${data[`keep${orderKeep}`].obj}%`;
-
-  const destroKeepStatus =
-    data[`keep${destroKeep}`].obj === 'Safe'
-      ? data[`keep${destroKeep}`].obj
-      : `${data[`keep${destroKeep}`].status} ${data[`keep${destroKeep}`].obj}%`;
-
-  let fields = [
-    {
-      name: 'AAO',
-      value: aao
-    },
-    {
-      name: 'Order',
-      value: `rank ${data[`keep${orderKeep}`].rank}\n${orderKeepStatus}\n${
-        data[`pop${orderKeep}`]
-      }`,
-      inline: true
-    },
-    {
-      name: 'Destruction',
-      value: `rank ${data[`keep${destroKeep}`].rank}\n${destroKeepStatus}\n${
-        data[`pop${destroKeep}`]
-      }`,
-      inline: true
-    }
-  ];
-
-  return new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(`${data.name}`)
-    .addFields(fields);
+const zoneLocks = (pairings = []) => {
+  return pairings.map((pairing) => {
+    return new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`${pairing.name}`)
+      .setDescription(
+        `${pairing.name} has been locked by **${pairing.owner}.**`
+      );
+  });
 };
 
-const fort = (data) => {
-  let orderPop = Forts[data.name] === 'Order' ? data.pop2 : data.pop1;
-  let destroPop = Forts[data.name] === 'Order' ? data.pop1 : data.pop2;
+const keep = (keeps = []) => {
+  return keeps.map((zone) => {
+    const aao =
+      zone.aaoOwner === 'Destruction' || zone.aaoOwner === 'Order'
+        ? `${zone.aao}% ${zone.aaoOwner}`
+        : zone.aaoOwner;
 
-  const fields = [
-    { name: 'Stage', value: data.stage },
-    { name: 'Health', value: data.health },
-    { name: 'Order', value: orderPop, inline: true },
-    { name: 'Destruction', value: destroPop, inline: true }
-  ];
+    const orderKeep = zone.keep1.owner === 'Order' ? 1 : 2;
+    const destroKeep = zone.keep1.owner === 'Order' ? 2 : 1;
 
-  return new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(`${data.name}`)
-    .setDescription(`Fort siege underway! Get there now!`)
-    .addFields(fields);
+    const orderKeepStatus =
+      zone[`keep${orderKeep}`].obj === 'Safe'
+        ? zone[`keep${orderKeep}`].obj
+        : `${zone[`keep${orderKeep}`].status} ${zone[`keep${orderKeep}`].obj}%`;
+
+    const destroKeepStatus =
+      zone[`keep${destroKeep}`].obj === 'Safe'
+        ? zone[`keep${destroKeep}`].obj
+        : `${zone[`keep${destroKeep}`].status} ${
+            zone[`keep${destroKeep}`].obj
+          }%`;
+
+    let fields = [
+      {
+        name: 'AAO',
+        value: aao
+      },
+      {
+        name: 'Order',
+        value: `rank ${zone[`keep${orderKeep}`].rank}\n${orderKeepStatus}\n${
+          zone[`pop${orderKeep}`]
+        }`,
+        inline: true
+      },
+      {
+        name: 'Destruction',
+        value: `rank ${zone[`keep${destroKeep}`].rank}\n${destroKeepStatus}\n${
+          zone[`pop${destroKeep}`]
+        }`,
+        inline: true
+      }
+    ];
+
+    return new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`${zone.name}`)
+      .addFields(fields);
+  });
 };
 
-const city = (data) => {
-  let index = 0;
+const fort = (forts = []) => {
+  return forts.map((zone) => {
+    const orderPop = Forts[zone.name] === 'Order' ? zone.pop2 : zone.pop1;
+    const destroPop = Forts[zone.name] === 'Order' ? zone.pop1 : zone.pop2;
 
-  for (let i = 0; i < data.length; i++) {
-    if (Cities.has(data[i].name)) {
-      index = i;
-      break;
-    }
-  }
+    const fields = [
+      { name: 'Stage', value: zone.stage },
+      { name: 'Health', value: zone.health },
+      { name: 'Order', value: orderPop, inline: true },
+      { name: 'Destruction', value: destroPop, inline: true }
+    ];
 
-  const embed = new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(data[index].name)
-    .setDescription(`${data[index].name} is under attack! Get there now!`);
+    return new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(`${zone.name}`)
+      .setDescription(`Fort siege underway! Get there now!`)
+      .addFields(fields);
+  });
+};
 
-  return embed;
+const city = (cities = []) => {
+  return cities.map((city) => {
+    return new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle(city.name)
+      .setDescription(`${city.name} is under attack! Get there now!`);
+  });
 };
 
 const sendReports = (message, reports) => {
